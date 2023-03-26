2948e38bf160bcc4c6a3f40af0390d68809e55a0
Asset1996 <khaitbay96@gmail.com>
2023-03-26 23:27:32 +0500

    fix

--- backend/Modules/System/Routes/api.php
+++ backend/Modules/System/Routes/api.php
@@ -16,9 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'system'], function() {
     Route::post('/login', 'AuthController@authenticate');
-
-    Route::middleware('auth:sanctum')->get('/test', function (Request $request) {
-        print_r(\Auth::user());
-        exit();
-    });
 });
